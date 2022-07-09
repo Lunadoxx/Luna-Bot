41352af36eb7ac534415c357dea2b87709b162f3
Amy <104725690+Lunadoxx@users.noreply.github.com>
2022-07-08 23:21:50 -0500

    Add files via upload

---
+++ Luna Bot.py
@@ -0,0 +1,26 @@
+import discord
+import json
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=";",intents=discord.Intents.all())
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(';help for a list of commands !!!'))
+    print('Boot successful')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("Pong !")
+
+@client.command()
+async def lunanetwork(ctx):
+    await ctx.send("<:lunanetwork:981940613069017138> https://sites.google.com/view/lunanetwork <:lunanetwork:981940613069017138>")
+
+@client.command()
+async def lunahub(ctx):
+    await ctx.send("<:lunahub:981940577748795473> **Here are all of the domains for Luna Hub** | https://www.lunahub.gq |"
+    " https://www.yousmellikefish.ml/ | https://sites.google.com/view/lunahub <:lunahub:981940577748795473>")
+
+client.run("OTgxNTU4Njg5OTc2Njg0NTg3.GmAWOw.a4QihNrPor8IrEZMwP8zRm5uq6bn_Y829R3D_g")
